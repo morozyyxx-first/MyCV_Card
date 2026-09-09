c2d3f7a62f9ef5e050ec78f50ea7a285eb2f972c
morozyyxx <morozyyxx@gmail.com>
2026-09-09 19:57:39 +0200

    .

--- MyCV_Card/src/Header.jsx
+++ MyCV_Card/src/Header.jsx
@@ -2,7 +2,7 @@
 function Header() {
     return (
         <header>
-            <img src="./assets/my_photo.jpg" alt="My_Photo" />
+            <img src="/src/assets/my_photo.jpg" alt="My_Photo" />
             <div id="head-info">
                 <h2>Viktor Moroz</h2>
                 <b>Profile:</b> Backend Python Developer / API Developer<br/>
