4031c2b35b7562bf459545975811de9ac8099448
morozyyxx <morozyyxx@gmail.com>
2026-09-09 19:29:18 +0200

    fix photo import

--- MyCV_Card/src/Header.jsx
+++ MyCV_Card/src/Header.jsx
@@ -1,9 +1,8 @@
-import myPhoto from 'src/assets/my_photo.jpg'
 
 function Header() {
     return (
         <header>
-            <img src={myPhoto} alt="My_Photo" />
+            <img src="/my_photo.jpg" alt="My_Photo" />
             <div id="head-info">
                 <h2>Viktor Moroz</h2>
                 <b>Profile:</b> Backend Python Developer / API Developer<br/>
